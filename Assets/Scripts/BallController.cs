33492dfa72b115e689fd390fea297fa1d13a12c7
phucduong <m.phuc2552000@gmail.com>
2024-11-21 16:52:16 +0700

    fix ball color bug

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     //public Color ballColor;
-    public ColorUtilsScript ballColorScript;
+    //public ColorUtilsScript ballColorScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ballColorScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<ColorUtilsScript>();
+        //ballColorScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<ColorUtilsScript>();
     }
 
     // Update is called once per frame
